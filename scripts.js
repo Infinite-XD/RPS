0c2ade3df12db1878d1f4cefb33d903e3c2dc829
Mustafa :p <mustafakhattak360@icloud.com>
2023-06-16 23:09:23 +0100

    Create a getComputerChoice function in scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    let x = Math.floor((Math.random() * 3) + 1);
+    if (x==1) {
+        return "rock"
+    }
+    else if (x==2) {
+        return "paper"
+    }
+    else {
+        return "scissors"
+    }
+}
